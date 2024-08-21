13510118f68bc407fc7945526a8afb8107be18fb
pruulmartti <marttipr@metropolia.fi>
2024-08-21 09:56:26 +0300

    lisätty tehtävä1 ja 2

---
+++ Moduuli 1/tehtävä5.py
@@ -0,0 +1,6 @@
+luku1=int(input('anna ensimmäinen kokonaisluku'))
+luku2=int(input('anna toinen kokonaisluku'))
+luku3=int(input('anna kolmas kokonaisluku'))
+print(f'{luku1}+{luku2}+{luku3}')
+print(f'{luku1}*{luku2}*{luku3}')
+print(f'{luku1}+{luku2}+{luku3}//3')
