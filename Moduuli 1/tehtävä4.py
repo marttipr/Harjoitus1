4c5a6feb2f6fd58cc7639ce8c4aba3d0576addb8
pruulmartti <marttipr@metropolia.fi>
2024-08-21 09:44:47 +0300

    lisätty tehtävä1 ja 2

---
+++ Moduuli 1/tehtävä4.py
@@ -0,0 +1,6 @@
+kanta = float(input('anna suorakulmion kanta'))
+korkeus= float(input('anna suorakulmion korkeus'))
+pinta_ala = kanta *korkeus
+piiri = 2*(kanta + korkeus)
+print(f'suorakulmion pinta-ala on''pinta_ala:.2f''neliöyksikköä')
+print(f'suorakulmion piiri on''piiri:.2f''neliöyksikköä')
