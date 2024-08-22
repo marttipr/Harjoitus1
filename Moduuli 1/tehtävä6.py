9a8dd297458344e8537dd16e9985374eecc5cb77
pruulmartti <marttipr@metropolia.fi>
2024-08-22 13:12:25 +0300

    lisätty toinen moduuli

---
+++ Moduuli 1/tehtävä6.py
@@ -0,0 +1,7 @@
+leviskät=int(input('anna massa levisköinä'))
+naulat=int(input('anna massa nauloina'))
+luoti=int(input('anna massa luotina'))
+luoti_grammoina=13.3
+naula_grammoina=32*luoti_grammoina
+leiviska_grammoina=20*naula_grammoina
+kokonaisgrammat = (leiviskat*leiviska_grammoina) + (naulat*naula_grammoina) + (luoti*luoti_grammoina)
