0e2f7fe66af6e7cd24a6013af131df7890177541
pruulmartti <marttipr@metropolia.fi>
2024-08-26 10:17:35 +0300

    lisätty toinen moduuli

--- Moduuli 2/tehtävä7.py
+++ Moduuli 2/tehtävä7.py
@@ -2,6 +2,7 @@
 import random
 random_number = random.randint(0, 9)
 print(f'satunnainen luku 0-9: {random_number}')
-#034
+
+
 random_number = random.randint(0, 6)
 print(f'satunnainen luku 0-6: {random_number}')
