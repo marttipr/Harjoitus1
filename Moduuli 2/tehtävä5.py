2946c9f0e9de1c0dabba0e67568e782af2114f58
pruulmartti <marttipr@metropolia.fi>
2024-08-26 10:08:01 +0300

    lisätty toinen moduuli

--- Moduuli 2/tehtävä5.py
+++ Moduuli 2/tehtävä5.py
@@ -2,6 +2,9 @@ import math
 luku1=int(input('anna ensimmäinen kokonaisluku'))
 luku2=int(input('anna toinen kokonaisluku'))
 luku3=int(input('anna kolmas kokonaisluku'))
-print(f'{luku1}+{luku2}+{luku3}')
-print(f'{luku1}*{luku2}*{luku3}')
-print(f'{luku1}+{luku2}+{luku3}//3')
+
+summa = luku1+luku2+luku3
+tulo = luku1*luku2*luku3
+keskiarvo = luku1+luku2+luku3/3
+print(f'lasketaan näiden lukujen summa {summa} tulo {tulo} ja keskiarvo {keskiarvo}')
+
