3bd8190d1828212c5117b6609103459ad2b3c20d
pruulmartti <marttipr@metropolia.fi>
2024-09-15 12:45:23 +0300

    lisätty toinen moduuli

--- Moduuli 7/mod7_harjoitus.py
+++ Moduuli 7/mod7_harjoitus.py
@@ -1,3 +1,4 @@
+'''
 # MONIKKO
 ###########
 
@@ -190,3 +191,23 @@ yhteystiedot = {
     'Ahmed': {'puh': '040-1112223', 'osoite': 'isotie 1'}
 
 print(f'Viivin osoite {yhteystiedot["Ahmed"]["osoite"]}')
+'''
+
+def anna_vuodenaika(kuukausi_numero):
+    vuodenaika_dict = {
+        'Kevät': {3, 4, 5},
+        'Kesä': {6, 7, 8},
+        'Syksy': {9, 10, 11},
+        'Talvi': {12, 1, 2}
+    }
+
+    for vuodenaika, kuukaudet in vuodenaika_dict.items():
+        if kuukausi_numero in kuukaudet:
+            return vuodenaika
+    return 'Virheellinen kuukausinumero'
+
+
+kuukausi_numero = int(input('Anna kuukausi numerona (1-12): '))
+vuodenaika = anna_vuodenaika(kuukausi_numero)
+print(f'Vuodenaika on {vuodenaika}')
+
